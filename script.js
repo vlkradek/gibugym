14d277a9e136a2d3ae00febfd719a282c5fc858f
vlkradek <radekvlk31@gmail.com>
2021-11-10 14:00:22 +0100

    vylepseni navbaru

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let burger = document.getElementById('burger')
 let navbar = document.getElementById('navbar')
+// let link = document.getElementsByTagName('li')
 
-navbar.style.right = '-250px';
+navbar.style.right = '-100vw';
+burger.innerHTML = '<i class=\'bx bx-menu\' ></i>';
 
 burger.onclick = function (){
-    if (navbar.style.right == '-250px'){
+    if (navbar.style.right == '-100vw'){
         navbar.style.right = '0'
+        burger.innerHTML = '<i class=\'bx bx-x\' ></i>'
     } else {
-        navbar.style.right = '-250px'
+        navbar.style.right = '-100vw'
+        burger.innerHTML = '<i class=\'bx bx-menu\' ></i>';
     }
+}
+function closeNavbarClick(){
+    navbar.style.right = '-100vw'
+    burger.innerHTML = '<i class=\'bx bx-menu\' ></i>';
 }
